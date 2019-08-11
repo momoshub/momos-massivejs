c986753bd8198e0c71bbfedcefc6a5a7014e1d85
Dian Fay <dian.m.fay@gmail.com>
2019-08-11 00:04:19 -0400

    fix: empty the entity cache on db.reload()

--- test/database/reload.js
+++ test/database/reload.js
@@ -4,7 +4,7 @@ describe('reload', function () {
   let db;
 
   before(function* () {
-    const initDb = yield resetDb('multi-schema');
+    const initDb = yield resetDb('foreign-keys');
 
     // reconnect with a pool size of 1 to make it easier to change runtime settings on all connections
     db = yield massive({
@@ -20,13 +20,26 @@ describe('reload', function () {
 
   after(() => db.instance.$pool.end());
 
+  it('expires the compound entity cache on db.reload()', async function () {
+    const a = db.alpha.join('beta');
+
+    await db.reload();
+
+    assert.isEmpty(db.entityCache);
+
+    const b = db.alpha.join('beta');
+
+    assert.notEqual(a, b);
+  });
+
   it('defaults currentSchema to "public"', () => {
     assert.equal(db.currentSchema, 'public');
   });
 
+  // this test case changes the search path and should remain last in the file
   it('picks up change in current schema', () => {
-    return db.query('SET search_path=test')
+    return db.query('SET search_path=sch')
       .then(() => db.reload())
-      .then(() => assert.equal(db.currentSchema, 'test'));
+      .then(() => assert.equal(db.currentSchema, 'sch'));
   });
 });
